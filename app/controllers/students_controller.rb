299e30323c8f7959fea09fafabf8807a8ca19e47
RebeccaHA <rebeccahunt034@gmail.com>
2020-04-21 19:56:31 +0100

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,13 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  
+  end
+
+  def create
+    session[params] = params
+    
+  end
+
 end
